1fa7d881e0605bf2af91ec9688c078de41b77fca
aishwarya-phatak <ashp17star@gmail.com>
2022-10-27 12:18:46 +0530

    Retrieving, Updating Data from Core Data

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       insertData()
+        insertData()
+        retriveRecordsFromCoreData()
+        updatingPersonRecord()
+        retriveRecordsFromCoreData()
     }
     
     func insertData(){
@@ -23,17 +26,51 @@ class ViewController: UIViewController {
         let managedContext = appDelegate.persistentContainer.viewContext
         let personEntity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
         
-        for i in 1...2{
+        for i in 1...3{
             let person = NSManagedObject(entity: personEntity, insertInto: managedContext)
-            person.setValue("Pratik", forKey: "name")
-            person.setValue(24, forKey: "age")
-            person.setValue(50000.00, forKey: "salary")
+            person.setValue("\(i)-Person", forKey: "name")
+            //person.setValue(Int32("\(i+20)"), forKey: "age")
+            //person.setValue((i * 50000), forKey: "salary")
         }
         
         do{
             try managedContext.save()
         } catch let error as NSError{
             print("Data cannot be saved\(error) -- \(error.userInfo)")
+        }
+    }
+    
+    func retriveRecordsFromCoreData(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
+        do{
+            let fetchResult = try managedContext.fetch(fetchRequest)
+            for eachRecordOfPerson in fetchResult as! [NSManagedObject]{
+                print(eachRecordOfPerson.value(forKey: "name") as! String)
+                //print(eachRecordOfPerson.value(forKey: "age") as! Int32)
+                //print(eachRecordOfPerson.value(forKey: "salary") as! Double)
+            }
+        } catch {
+            print("Failed to fetch records from person database")
+        }
+    }
+    
+    func updatingPersonRecord(){
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else{
+            return }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequestForUpdateQuery : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Person")
+        fetchRequestForUpdateQuery.predicate = NSPredicate(format: "name = %@", "1-Person")
+        do{
+            let update = try managedContext.fetch(fetchRequestForUpdateQuery)
+            let personObjectToBeUpdated = update[0] as! NSManagedObject
+            personObjectToBeUpdated.setValue("4-Person", forKey: "name")
+            personObjectToBeUpdated.setValue(25, forKey: "age")
+            personObjectToBeUpdated.setValue(70454.34, forKey: "salary")
+        }catch{
+            print("record not updated")
         }
     }
 }
